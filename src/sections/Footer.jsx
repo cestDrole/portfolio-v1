4d88ea4a33805da4e59d6688ea6939e97650f76e
SJ <smwjasnosz@gmail.com>
2023-12-31 13:48:33 +0100

    Fix footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,11 +1,9 @@
 import { githubWhite, linkedinWhite } from "../assets";
 
 const Footer = () => (
-  <footer className='flex flex-auto justify-between bg-slate-900 h-[100px] rounded-t-2xl items-center'>
-    <h2 className='text-white font-montserrat pl-4'>
-      © Sebastian Jasnosz 2023
-    </h2>
-    <div className='flex flex-1 gap-6 items-end justify-center'>
+  <footer className=' bg-slate-900 h-[80px] rounded-t-2xl flex items-center justify-between px-10 sm:justify-center sm:gap-14 sm:px-0'>
+    <h2 className='text-white font-montserrat'>© Sebastian Jasnosz 2023</h2>
+    <div className='flex justify-center items-center gap-4'>
       <a href='https://github.com/cestDrole' className='cursor-pointer'>
         <img src={githubWhite} className='h-7 w-7' />
       </a>
