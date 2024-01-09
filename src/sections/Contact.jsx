6aa23245bd83346d9684be3211b08a95fd2a4813
SJ <smwjasnosz@gmail.com>
2024-01-09 18:04:35 +0100

    Contact secion animations

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,12 +1,26 @@
 import { pin, mail } from "../assets";
+import { motion } from "framer-motion";
+import { textVariant, textContainer } from "../utils/motion";
 
 const Contact = () => (
-  <div className='flex flex-1 items-center justify-center flex-col gap-8 my-8'>
-    <h1 className='text-center font-semibold text-3xl font-montserrat'>
+  <motion.div
+    variants={textContainer}
+    initial='hidden'
+    whileInView='show'
+    viewport={{ once: true, amount: 0.5 }}
+    className='flex flex-1 items-center justify-center flex-col gap-8 my-8'
+  >
+    <motion.h1
+      variants={textVariant(0.3)}
+      className='text-center font-semibold text-3xl font-montserrat'
+    >
       Let's get in touch!
-    </h1>
+    </motion.h1>
     <div className='flex flex-1 items-center justify-center flex-col md:flex-row gap-8'>
-      <div className='flex flex-col justify-center items-center gap-2 h-[93px] w-[167px]'>
+      <motion.div
+        variants={textVariant(0.4)}
+        className='flex flex-col justify-center items-center gap-2 h-[93px] w-[167px]'
+      >
         <img src={mail} className='h-7 w-7' />
         <h2 className='text-xl font-medium font-montserrat'>Mail</h2>
 
@@ -16,14 +30,17 @@ const Contact = () => (
         >
           smwjasnosz@gmail.com
         </a>
-      </div>
-      <div className='flex flex-col justify-center items-center gap-2 h-[93px] w-[167px]'>
+      </motion.div>
+      <motion.div
+        variants={textVariant(0.5)}
+        className='flex flex-col justify-center items-center gap-2 h-[93px] w-[167px]'
+      >
         <img src={pin} className='h-7 w-7' />
         <h2 className='text-xl font-medium font-montserrat'>Location</h2>
 
         <p className='font-palanquin'>Gdańsk, Poland</p>
-      </div>
+      </motion.div>
     </div>
-  </div>
+  </motion.div>
 );
 export default Contact;
