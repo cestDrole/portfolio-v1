7169cc500fcd9c271102f4e68c628d6dbe80fd27
SJ <smwjasnosz@gmail.com>
2024-01-11 18:58:04 +0100

    Image optimization

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,26 +2,26 @@ import hamburger from "./hamburger.svg";
 import Star from "./Star.svg";
 import download from "./download.svg";
 import github from "./github.svg";
-import linkedin from "./linkedin.png";
+import linkedin from "./linkedin.webp";
 import html from "./html.svg";
 import css from "./css.svg";
 import javascript from "./javascript.svg";
 import typescript from "./typescript.svg";
 import react from "./reactjs-icon.svg";
 import tailwind from "./tailwind.svg";
-import manager from "./manager.png";
-import developer from "./developer.png";
-import shoes from "./shoes.png";
-import hoobank from "./hoobank.png";
-import carRent from "./carRent.png";
-import metaverse from "./metaverse.png";
+import manager from "./manager.webp";
+import developer from "./developer.webp";
+import shoes from "./shoes.webp";
+import hoobank from "./hoobank.webp";
+import carRent from "./carRent.webp";
+import metaverse from "./metaverse.webp";
 import external from "./external.svg";
 import githubWhite from "./github-white.svg";
 import linkedinWhite from "./linkedin-white.svg";
 import pin from "./pin.svg";
 import mail from "./mail.svg";
 import close from "./close.svg";
-import hero from "./hero.jpg";
+import hero from "./hero.webp";
 
 export {
   hamburger,
